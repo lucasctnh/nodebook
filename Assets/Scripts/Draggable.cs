2a1fc6103250603ba13beb4dee9f0a09f11292e0
Lucas Coutinho <lcscoutinho@outlook.com>
2023-12-14 22:55:05 -0300

    Add initial tests for infinite canvas

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -5,6 +5,7 @@ using UnityEngine.EventSystems;
 
 public class Draggable : MonoBehaviour, IDragHandler
 {
+	public RectTransform content;
 	private Canvas canvas;
 	private RectTransform rectTransform;
 
@@ -13,9 +14,46 @@ public class Draggable : MonoBehaviour, IDragHandler
 		canvas = GetComponentInParent<Canvas>();
 		rectTransform = GetComponent<RectTransform>();
 	}
+	private void Update()
+	{
+		if (IsRectTransformInsideContent(content, rectTransform))
+		{
+			Debug.Log("Element is contained in Content");
+		}
+		else
+		{
+			Debug.Log("Element is not contained in Content");
+			GrowContentSize();
+		}
+	}
 
 	public void OnDrag(PointerEventData eventData)
 	{
 		rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+	}
+
+	private bool IsRectTransformInsideContent(RectTransform content, RectTransform rectTransform)
+	{
+		Vector3[] corners = new Vector3[4];
+		rectTransform.GetWorldCorners(corners);
+
+		foreach (Vector3 corner in corners)
+		{
+			// I am not sure (its not documented) but I think that if dont pass
+			// the Camera param Unity will convert into world space
+			// so, if you pass the param you need to first convert the points into Screen space
+			// Vector3 screenCorner = Camera.main.WorldToScreenPoint(corner);
+
+			if (!RectTransformUtility.RectangleContainsScreenPoint(content, corner))
+				return false;
+		}
+
+		return true;
+	}
+
+	private void GrowContentSize()
+	{
+		// just a test
+		content.sizeDelta += new Vector2(50, 50);
 	}
 }
